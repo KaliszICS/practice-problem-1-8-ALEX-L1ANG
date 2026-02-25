6e386ad9914734296cc19dd53f8981acaa3d04b8
ALEX-L1ANG <343293684@tdsb.ca>
2026-02-25 15:06:22 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,8 @@
 /**
- * File Name: errors.java
- * Author: Mr. Kalisz
- * Date Created: March 29, 2019
- * Date Last Edited: March 29, 2019
+ * File Name: Errors and Exceptions
+ * Author: Alex Liang
+ * Date Created: Feb 24, 2026
+ * Date Last Edited: Feb 25, 2026
  */
 
 
@@ -18,22 +18,24 @@ class PracticeProblem {
   	}
   	public static void q1() {
 		// From input, recieve two integers from the user and add them together.  Output the result.
-
+	Scanner s = new Scanner(System.in);
 		System.out.println("Input a number: ");
-		num1 = s.nextInt;
+		int num1 = s.nextInt;
 		System.out.println("Input a number: ");
-		num2 = s.nextInt;
+		int num2 = s.nextInt;
 
 		System.out.println(num + num2);
   	}
 
 	public static void q2() {
 		// From input recieve two integers.  Output the quotient rounded down.
-		
+	Scanner s = new Scanner(System.in);
 		System.out.println("Input a number: ");
-		num1 = s.nextLine();
+		int num1 = s.nextInt();
+		s.nextLine();
 		System.out.println("Input a number: ");
-		num2 = s.nextLine();
+		int num2 = s.nextInt();
+		s.nextLine();
 		
 		system.out.println(num1/num2);
 	}
@@ -41,18 +43,20 @@ class PracticeProblem {
 	public static void q3() {
 		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?"
 		
-		System.out.println(hello "Mr. Kalisz" have you seen my work yet?);
+		System.out.println("hello Mr. Kalisz have you seen my work yet?");
 	}
 
 	public static void q4() {
 		// From input recieve two numbers (can be decimal fractions).  
 		// Output the first number plus two, then multiplied by the second number.
-		
+	Scanner s = new Scanner(System.in);
 		System.out.println("Input a number: ");
-		s.nextInt();
+		double num1 = s.nextDouble();
+		s.nextLine();
 		System.out.println("Input a number: ");
-		num2 = s.nextInt();
+		double num2 = s.nextDouble();
+		s.nextLine();
 		
-		System.out.println(num1 + 2 * num2)
+		System.out.println(num1 + 2 * num2);
 	}
 }
